4480240da95c254041f66408688563027de74c3f
YouttyLe <se184475leminhtuan@gmail.com>
2024-06-15 21:34:43 +0700

    feature/re-fix sliding_window

--- Module1/WEEK_2_DSA/max_num_in_k_sliding_window..py
+++ Module1/WEEK_2_DSA/max_num_in_k_sliding_window..py
@@ -11,7 +11,7 @@ def max_num_sliding_window(num_list, k):
 
 
 if __name__ == "__main__":
-    num_list = [3, 4, 5, 1, -44, 5, 10, 12, 33, 1]
+    num_list = [3 , 4 , 5 , 1 , -44 , 5 ,10 , 12 ,33 , 1]
     k = int(input(''))
 
     result = max_num_sliding_window(num_list, k)
